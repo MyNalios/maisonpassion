ac77a983e2bfbd4592691e7bcd962b57e57c9608
DWA <dwa@idealisconsulting.com>
2020-08-19 18:23:01 +0200

    [FIX] fixing bug conversion lead to opportunity

--- mp_crm/models/crm_lead.py
+++ mp_crm/models/crm_lead.py
@@ -10,20 +10,31 @@ class Lead(models.Model):
     referred_partner_ids = fields.Many2many('res.partner', 'crm_referred_partner_rel', 'lead_id', 'partner_id', string='Referred By')
 
     def write(self, vals):
-        if self.type == 'lead':
-            vals.update(self._get_partner_vals_from_email(vals.get('email_from')))  # overwrite existing keys
+        if (vals.get('type') == 'lead' or self.type == 'lead') \
+                and (vals.get('email_from') or vals.get('phone') or vals.get('mobile')) \
+                and not self.env.context.get('no_contact_synchronization'):
+            vals.update(self._get_partner_vals_from_email(vals.get('email_from'), vals.get('phone'), vals.get('mobile')))  # overwrite existing keys
         return super(Lead, self).write(vals)
 
     @api.model
     def create(self, vals):
-        if vals.get('type') == 'lead':
-            vals.update(self._get_partner_vals_from_email(vals.get('email_from')))  # overwrite existing keys
+        if vals.get('type') == 'lead' \
+                and (vals.get('email_from') or vals.get('phone') or vals.get('mobile')) \
+                and not self.env.context.get('no_contact_synchronization'):
+            vals.update(self._get_partner_vals_from_email(vals.get('email_from'), vals.get('phone'), vals.get('mobile')))  # overwrite existing keys
         return super(Lead, self).create(vals)
 
-    def _get_partner_vals_from_email(self, email):
-        if email:
-            partner = self.env['res.partner'].search([('email', '=', email)])
-            if partner:
+    def _get_partner_vals_from_email(self, email=None, phone=None, mobile=None):
+        domain = []
+        if email or self.email_from:
+            domain.append(('email', '=', email or self.email_from))
+        if phone or self.phone:
+            domain.append(('phone', '=', phone or self.phone))
+        if mobile or self.mobile:
+            domain.append(('mobile', '=', mobile or self.phone))
+        if domain:
+            partner = self.env['res.partner'].search(domain)
+            if len(partner) == 1:
                 onchange_values = self._onchange_partner_id_values(partner.id)
                 onchange_values.update({
                     'partner_id': partner.id,
