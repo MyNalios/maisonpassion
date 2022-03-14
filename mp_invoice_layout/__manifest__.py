6c7d5b851f3b779363674a41bf283f7f9bea6475
Abdulhamid ali <aab@idealisconsulting.com>
2022-03-14 16:36:18 +0100

    MP-62

--- mp_invoice_layout/__manifest__.py
+++ mp_invoice_layout/__manifest__.py
@@ -12,6 +12,7 @@
         'report/account_move_templates.xml',
         'views/account_move_views.xml',
         'views/sale_advance_payment_inv_views.xml',
+        'views/account_tax_from_view.xml',
     ],
     'installable': True,
     'auto_install': False,
--- mp_invoice_layout/models/__init__.py
+++ mp_invoice_layout/models/__init__.py
@@ -3,4 +3,5 @@
 from . import account_move_line
 from . import account_move
 from . import sale_advance_payment_inv
+from . import account_tax
 
