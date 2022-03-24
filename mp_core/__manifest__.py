b707a22086c103e95a3e271b9d83c0d2160ffd24
tresor-ruph <tresortek9@gmail.com>
2022-03-24 12:59:24 +0100

    migration module mp_core

--- mp_core/__manifest__.py
+++ mp_core/__manifest__.py
@@ -5,10 +5,17 @@
     'summary': 'Add Logic in the different models of Odoo in order to correspond with the way of working of the Maison Passion company.',
     'author': 'Idealis Consulting',
     'website': 'http://www.idealisconsulting.com',
-    'depends': [],
+    'depends': ['contacts', 'sale_management', 'crm', 'phone_validation'],
     'price': 0,
     'currency': 'EUR',
-    'data': [],
+    'data': [
+        'data/mp_ref_partner_sequence.xml',
+        'data/mp_phone_format_cron.xml',
+        'data/res_partner_data.xml',
+        'views/mp_res_partner_view.xml',
+        'views/mp_sale_order_view.xml',
+        'views/ir_qweb_widget_templates.xml'
+    ],
 
     'installable': True,
 }
