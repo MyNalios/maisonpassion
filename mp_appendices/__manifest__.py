4556875b7156a27a108e03d934fd23faf121f237
tresor-ruph <tresortek9@gmail.com>
2022-03-25 10:39:16 +0100

    migration module mp_appendices

--- mp_appendices/__manifest__.py
+++ mp_appendices/__manifest__.py
@@ -1,13 +1,22 @@
+# -*- coding: utf-8 -*-
+
 {
     'name': "Maison Passion Portal Appendices",
-    'version': '15.0',
+    'version': '13.0.0.1',
     'category': 'Operations/Helpdesk',
     'summary': 'Add Technical Documents for Partners to Portal',
     'sequence': 90,
     'author': 'dwa@idealisconsulting - Idealis Consulting',
     'website': 'http://www.idealisconsulting.com',
-    'depends': [],
-    'data': [],
+    'depends': ['portal', 'mp_core'],
+    'data': [
+        'views/sale_technical_document_tag_views.xml',
+        'views/sale_technical_document_templates.xml',
+        'views/sale_technical_document_views.xml',
+        'wizard/manage_technical_document_views.xml',
+        'views/res_partner_views.xml',
+        'security/ir.model.access.csv',
+    ],
     'auto_install': False,
     'installable': True,
     'application': True,
