c7c236cecc2906866429378311fb7daedcfad888
Pierre Locus <lop@nalios.be>
2023-06-01 16:17:29 +0200

    [FIX] Remove obligation of Balance line for personalized payment term

--- mp_custos/models/payment_term.py
+++ mp_custos/models/payment_term.py
@@ -1,12 +1,22 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields
+from odoo import models, fields, api, _
+from odoo.exceptions import ValidationError
 
 
 class PaymentTerm(models.Model):
     _inherit = 'account.payment.term'
 
     is_personalized = fields.Boolean('Is a personalized distribution')
+
+    @api.constrains('line_ids')
+    def _check_lines(self):
+        for terms in self:
+            if not terms.is_personalized and  len(terms.line_ids.filtered(lambda r: r.value == 'balance')) != 1:
+                raise ValidationError(_('The Payment Term must have one Balance line.'))
+            if terms.line_ids.filtered(lambda r: r.value == 'fixed' and r.discount_percentage):
+                raise ValidationError(_("You can't mix fixed amount with early payment percentage"))
+
 
 class PaymentTermLine(models.Model):
     _inherit = 'account.payment.term.line'
