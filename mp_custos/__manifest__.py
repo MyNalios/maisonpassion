8265a41fabfb2d36c308279358d1214d5e7659eb
lde-nalios <lde@nalios.be>
2023-06-01 11:15:26 +0200

    clean mp_custos module

--- mp_custos/__manifest__.py
+++ mp_custos/__manifest__.py
@@ -15,7 +15,7 @@
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/16.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Maison Passion',
+    'category': 'Uncategorized',
     'version': '16.0.1',
 
     # any module necessary for this one to work correctly
@@ -23,12 +23,6 @@
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
         # 'views/crm_lead_views.xml',
-        'views/templates.xml',
-    ],
-    # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
     ],
 }
