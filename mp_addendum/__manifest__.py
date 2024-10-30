859790e31ae10172e0508f75eeb043d44ca93435
lde-nalios <lde@nalios.be>
2024-10-30 19:13:57 +0100

    [fix] V17 custom code

--- mp_addendum/__manifest__.py
+++ mp_addendum/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Maison Passion Addendum',
-    'version': '13.0.0.1',
+    'version': '17.0.0.1',
     'author': 'Idealis Consulting',
     'category': 'Tools',
     'description': 'Module allowing to merge pdfs with existing reports',
